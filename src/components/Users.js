291dc0d34c5244e0df4d1ab84ab12b74055e20cd
ArhioTegio <Arhio@yandex.ru>
2024-10-20 18:07:15 +0300

    ReactJS - itproger.com - Урок 8

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,35 +1,14 @@
 import React from 'react';
+import User from './User';
 
 
 class Users extends React.Component {
-    users = [
-        {
-            id: 1,
-            firstname: 'Bob',
-            lastname: 'Marley',
-            bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-            age: 40,
-            isHappy: true
-        },
-        {
-            id: 2,
-            firstname: 'John',
-            lastname: 'Doe',
-            bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-            age: 22,
-            isHappy: false
-        }
-    ]
     render() {
-        if(this.users.length > 0) {
+        if(this.props.users.length > 0) {
             return (
                 <div>
-                    {this.users.map((user) => (
-                        <div className='user' key={user.id}>
-                            <h3>{user.firstname} {user.lastname}</h3>
-                            <p>{user.bio}</p>
-                            <b>{user.isHappy ? 'Счастлив :)' : 'Не особо :('}</b>
-                        </div>
+                    {this.props.users.map((user) => (
+                        <User  key={user.id} user={user} />
                     ))}
                 </div>
             )
