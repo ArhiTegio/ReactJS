237fcaffdfd5ea52b5ddbf3acffe6f5186e9f1f9
ArhioTegio <Arhio@yandex.ru>
2024-10-23 10:21:08 +0300

    ReactJS - itproger.com - Урок 9

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header3 from './components/Header';
 import Users from './components/Users'
 import AddUser from './components/AddUser';
+import axios from 'axios';
+
+const baseUrl = 'https://reqres.in/api/users?page=1'
 
 function Header1() {
     return (<header>Шапка сайта</header>)
@@ -14,27 +17,19 @@ const Header2 = () => {
 class App extends React.Component{
   constructor(props) {
       super(props)
+
+      axios.get(baseUrl).then((res) =>
+      {
+        this.setState({
+          users: res.data.data
+        })
+      })
       this.state = {
-          users:[
-              {
-                  id: 1,
-                  firstname: 'Bob',
-                  lastname: 'Marley',
-                  bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                  age: 40,
-                  isHappy: true
-              },
-              {
-                  id: 2,
-                  firstname: 'John',
-                  lastname: 'Doe',
-                  bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                  age: 22,
-                  isHappy: false
-              }
-          ]
+          users:[]
       }
       this.addUser = this.addUser.bind(this)
+      this.deleteUser = this.deleteUser.bind(this)
+      this.editUser = this.editUser.bind(this)
   }
   
     
@@ -42,13 +37,28 @@ class App extends React.Component{
         return (<div className="name">
             <Header3 title="Список пользователей"/>
             <main>
-              <Users users={this.state.users}/>
+              <Users users={this.state.users} onEdit={this.editUser} onDelete={this.deleteUser}/>
             </main>
             <aside>
               <AddUser onAdd={this.addUser}/>
             </aside>
         </div>)
   } 
+
+  deleteUser(id) {
+      this.setState({
+        users: this.state.users.filter((el) => el.id !== id)
+      })
+  }
+
+  editUser(user) {
+    let allUsers = this.state.users
+    allUsers[user.id - 1] = user
+
+    this.setState({ users: []}, () =>{
+      this.setState({users: [...allUsers]})
+    })
+  }
 
   addUser(user) {
     const id = this.state.users.length + 1
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 
 class AddUser extends React.Component {
+    UserAdd = {}
     constructor(props) {
         super(props)
-        this.state = {
-            firstname: '',
-            lastname: '',
-            bio: '',
+        
+        this.state = props.user ? props.user : {
+            first_name: '',
+            last_name: '',
+            email: '',
             age: 1,
             isHappy: false
         }
@@ -16,20 +18,26 @@ class AddUser extends React.Component {
 
     render() {
         return (
-            <form>
-                <input placeholder='Имя' onChange={(e) => this.setState({ firstname: e.target.value})} />
-                <input placeholder='Фамилия' onChange={(e) => this.setState({ lastname: e.target.value})} />
-                <textarea placeholder='Биография' onChange={(e) => this.setState({ bio: e.target.value})}></textarea>
-                <input placeholder='Возраст' onChange={(e) => this.setState({ age: e.target.value})}/>
+            <form ref={(el) => this.myForm = el}>
+                <input placeholder='Имя' value={this.state.first_name} onChange={(e) => this.setState({ firstname: e.target.value})} />
+                <input placeholder='Фамилия' value={this.state.last_name} onChange={(e) => this.setState({ lastname: e.target.value})} />
+                <textarea placeholder='Биография' value={this.state.email} onChange={(e) => this.setState({ email: e.target.value})}></textarea>
+                <input placeholder='Возраст' value={this.state.age} onChange={(e) => this.setState({ age: e.target.value})}/>
                 <label htmlFor="isHappy">Счастлив?</label>
-                <input type='checkbox' id='isHappy' placeholder='Имя' onChange={(e) => this.setState({ isHappy: e.target.checked})} />
-                <button type="button" onClick={() => this.props.onAdd({
-                        firstname: this.state.firstname,
-                        lastname: this.state.lastname,
-                        bio: this.state.bio,
+                <input type='checkbox' id='isHappy' placeholder='Счастлив' checked={this.state.isHappy} onChange={(e) => this.setState({ isHappy: e.target.checked})} />
+                <button type="button" onClick={() => {
+                    this.myForm.reset()
+                    this.UserAdd = {
+                        first_name: this.state.first_name,
+                        last_name: this.state.last_name,
+                        email: this.state.email,
                         age: this.state.age,
                         isHappy: this.state.isHappy
-                    })}>Добавить</button>
+                    }
+                    if(this.props.user)
+                        this.UserAdd.id = this.props.user.id
+                    this.props.onAdd(this.UserAdd)}
+                    }>Добавить</button>
             </form>
         )
     }
